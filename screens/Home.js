6ddd5438164ec79762d5e8fd8e26c171af1af966
Brow1230 <43067741+Brow1230@users.noreply.github.com>
2021-10-17 10:05:48 -0400

    logo on homescreen showing

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Text, Box, VStack, Button, Divider } from 'native-base';
+import { Image, Text, Box, VStack, Button, Divider } from 'native-base';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { SvgXml } from 'react-native-svg';
+import ShakepayLogo from '../assets/ShakepayLogo.svg'
+
 
 export default function HomeScreen() {
   return (
           <VStack>
-            <Box width="100%" height="40%" justifyContent="center" backgroundColor='white'>
+            <Box width="100%" height="60%" justifyContent="center" backgroundColor='white' alignItems="center">
+              <ShakepayLogo width={40} height={40} />
               <Text fontSize='36' textAlign="center"  >  $1.88 </Text>
-            </Box>
-            <Button.Group mx="auto" >
+              <Button.Group mx="auto" marginTop='30' >
               <Button size="lg"  width="45%" >Add Funds</Button>
               <Button size="lg" width="45%" >Send</Button>
             </Button.Group>
+            </Box>
             <Divider/>
           </VStack>
   )};
